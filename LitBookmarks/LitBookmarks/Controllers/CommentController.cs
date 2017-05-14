d732a3decda3d2bbebc8dcc4a069928f7e425968
Ulyana Zhovtanetska <ulyana.zhovtanetska@gmail.com>
2017-05-15 02:39:33 +0300

    Added comment icon

---
+++ LitBookmarks/LitBookmarks/Controllers/CommentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain_Logic.Abstract;
+
+namespace LitBookmarks.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CommentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Comment()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
